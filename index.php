f369cdd4d821ce31246be7b7161513ac57fa90bd
Vin-EternalV <vjkrauss428@gmail.com>
2025-10-03 13:47:04 +0800

    Update index

--- index.php
+++ index.php
@@ -4,9 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medical Inquary and Inventory System</title>
+    <link rel="stylesheet" href="design.css">
 </head>
 <body>
     
+    
+
+           <nav>
+
+             <h1 id="title">MIIS</h1>
+
+            <ul>
+
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+
+            </ul>
+
+
+           </nav>
+
+
+
+           <div class="home">
+
+
+                <div class="left-text">
+                    <h1>Welcome to MIIS</h1>
+                    <p>Medical Inquary and Inventory System</p>
+                   
+                </div>
+
+               
+
+
+
+
+           </div>
+
+
+
 </body>
 </html>
 
